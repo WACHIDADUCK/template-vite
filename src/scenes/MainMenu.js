9fe97d401d9af4e15746c65ed288ca437753f191
WACHIDADUCK <captainvantruc@gmail.com>
2025-01-29 19:38:51 +0000

    añadiendo yml

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -7,17 +7,38 @@ export class MainMenu extends Scene
         super('MainMenu');
     }
 
+
+    preload ()
+    {   
+        this.load.image('bg', 'assets/bg.png');
+        this.load.spritesheet('ovni', 'assets/ovni.png', { frameWidth: 595, frameHeight: 250 });
+    }
+
     create ()
     {
-        this.add.image(512, 384, 'background');
+        // this.add.image(512, 384, 'background');
+        // this.add.image(512, 300, 'logo');
+        this.load.image('bg', 'assets/bg.png');
 
-        this.add.image(512, 300, 'logo');
 
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        // ovni.animation.add('fly', [0, 1, 2, 3], 4, true);
+
+        this.anims.create({
+            key: 'fly',
+            frames: this.anims.generateFrameNumbers('ovni', { frames: [0, 1, 2, 3] }),
+            frameRate: 6,
+            repeat: -1
+        })
+        this.playser = this.add.sprite(512, 384, 'ovni');
+        this.playser.play('fly');
+
+        
+
+        // this.add.text(512, 460, 'Main Menu', {
+        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        //     stroke: '#000000', strokeThickness: 8,
+        //     align: 'center'
+        // }).setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
 
